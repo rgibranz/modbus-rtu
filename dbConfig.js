3f0f8d104a101b2eb05d7b34c42e5783b1b12e1f
rgibranz <rgibran234@gmail.com>
2023-11-09 16:52:50 +0700

    berhasil save data

--- dbConfig.js
+++ dbConfig.js
@@ -1,8 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const dbFile = __dirname + "/db/playlist.db";
 
-const db = new sqlite3.Database(dbFile, sqlite3.OPEN_READWRITE, (err) => {
-    if(err) throw err;
-});
+const db = new sqlite3.Database(dbFile);
 
 module.exports = db;
--- index.js
+++ index.js
@@ -3,31 +3,50 @@ const ModbusRTU = require("modbus-serial");
 const client = new ModbusRTU();
 const SerialPort = "/dev/tty.usbserial-A10LKGM2";
 
+const sqlite3 = require('sqlite3').verbose();
+const dbFile = __dirname + "/db/playlist.db";
+const db = new sqlite3.Database(dbFile);
+
 const connect = async (SerialPort) => {
   if (SerialPort) {
     try {
       await client.connectRTUBuffered(SerialPort, { baudRate: 9600 });
       client.setID(1);
     } catch (err) {
-      console.log(err);
+      console.log(Date().toString(),' : ', err);
     }
   }
 };
 
-let count = 1;
-connect(SerialPort);
+const read = async () => {
+  try {
+    let data = await client.readDiscreteInputs(0, 8);
 
-setInterval(() => {
-  client.readDiscreteInputs(0, 8, (err, data) => {
-    console.log(`<==== Hitung Ke ${count} ====>`);
-    if (data) {
-      const binaryData = data.data.map((value) => (value ? 1 : 0)).join("");
-      const hex = data.buffer.toString("hex")
-      console.log({hex:hex, binary:binaryData});
-    } else {
-      console.log(err.message);
-      connect(SerialPort);
-    }
-    count++;
+    const binary = data?.data?.map((value) => (value ? 1 : 0)).join("");
+    const hex = data?.buffer?.toString("hex");
+    
+    return { hex, binary };
+  } catch (err) {
+    connect(SerialPort);
+    console.log(Date().toString(),' : ', err.message);
+  }
+};
+
+
+const saveData = async (hex, biner) => {
+  let query = `INSERT INTO data_modbus (hex,biner,timestamp) VALUES('${hex}','${biner}','${Math.floor(Date.now() / 1000)}')`;
+  
+  db.serialize(() => {
+    db.run(query);
   });
+
+  console.log('Data Saved', Math.floor(Date.now() / 1000));
+}
+
+setInterval(async () => {
+  let data = await read();
+  
+  if(data){
+    saveData(data.hex,data.binary)
+  }
 }, 1000);
