c3bb6d99aa881f2b4f924f39ddef12703b4e1ed3
rgibranz <rgibran234@gmail.com>
2023-11-28 21:55:50 +0700

    multiple slave

---
+++ multiple-slave.js
@@ -0,0 +1,39 @@
+let ModbusRTU = require("modbus-serial");
+
+const address = "COM5"; 
+const startAddress = 0; 
+const quantity = 8; 
+
+const {saveModbusData} = require('./database');
+
+function connectAndReadData(unitId) {
+  let client = new ModbusRTU();
+
+  
+  client.connectRTUBuffered(address, { baudRate: 9200 }, (err) => {
+    if (err) {
+      console.error(`Connection error Slave ${unitId}:`, err.message);
+      saveModbusData('Conn Error',err.message);
+    } else {
+      client.setID(unitId);
+      client.setTimeout(50);
+
+      client.readDiscreteInputs(startAddress, quantity, (readErr, data) => {
+        if (readErr) {
+          console.error(`Read error slave ${unitId}:`, readErr.message);  
+          saveModbusData('Read Error',readErr.message );
+        } else { 
+          let binary = data?.data?.map((value) => (value ? 1 : 0)).join("");
+          console.log(`Succes Read slave ${unitId}:`, binary);
+          saveModbusData('Success','Success Save Data',binary);
+        }
+        client.close(() => {
+          
+        });
+      });
+    }
+  });
+}
+
+setTimeout(()=>connectAndReadData(1),50)
+setTimeout(()=>connectAndReadData(2),100);
