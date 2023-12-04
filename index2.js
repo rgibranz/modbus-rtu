d1b81a624baeeb1a8bfa0789e12da92370442a1b
rgibranz <rgibran234@gmail.com>
2023-12-04 15:32:35 +0700

    update logic

--- index2.js
+++ index2.js
@@ -1,72 +1,83 @@
 let ModbusRTU = require("modbus-serial");
 
-const address = "COM2";
+const address = "COM3";
 const startAddress = 0;
 const quantity = 8;
 const { saveModbusData } = require("./database");
 
-async function readDataWithID(client, id) {
+// fungsi untuk memmbaca data dengan Discrete Inputs
+async function readData(client, id) {
   return new Promise((resolve, reject) => {
-    const start = Date.now(); 
+    const start = Date.now();
     client.setID(id);
     client.setTimeout(1000);
     client.readDiscreteInputs(startAddress, quantity, (readErr, data) => {
       if (readErr) {
         console.error(`Read error for ID ${id}:`, readErr.message);
         saveModbusData(`Read Error ${id}`, readErr.message);
-        reject(readErr);
       } else {
         let binary = data?.data?.map((value) => (value ? 1 : 0)).join("");
         console.log(`Success Read ${id}:`, binary);
         saveModbusData(`Success ${id}`, "Success Save Data", binary);
 
-        const end = Date.now(); 
-        const duration = end - start;
-        console.log(`Membaca ID ${id} Dalam: ${duration}ms`);
         resolve(binary);
       }
+      const end = Date.now();
+      const duration = end - start;
+      console.log(`Membaca ID ${id} Dalam: ${duration}ms`);
+      console.log(`--------------------------`);
     });
   });
 }
 
-async function connectAndReadDataSequentially() {
+async function connectModbus(client) {
+  await new Promise((resolve, reject) => {
+    const start = Date.now();
+    client.connectRTUBuffered(address, { baudRate: 9600 }, (err) => {
+      if (err) {
+        console.error("Connection error:", err.message);
+        saveModbusData("Conn Error", err.message);
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+    const end = Date.now();
+    const duration = end - start;
+    console.log(`Konek Dalam: ${duration}ms`);
+    console.log(`--------------------------`);
+  });
+}
+
+async function main() {
   let client = new ModbusRTU();
 
   try {
-    await new Promise((resolve, reject) => {
-      client.connectRTUBuffered(address, { baudRate: 9600 }, (err) => {
-        if (err) {
-          console.error("Connection error:", err.message);
-          saveModbusData("Conn Error", err.message);
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    });
-
     const start = Date.now();
+    await connectModbus(client);
+    await readData(client, 1);
+    await readData(client, 2);
 
-    await readDataWithID(client, 1);
-    await readDataWithID(client, 2);
-    await readDataWithID(client, 3);
-    
     const end = Date.now();
     const elapsed = end - start;
     const remainingTime = 1000 - elapsed;
 
     if (remainingTime > 0) {
+      console.log(`Waktu Terpakai ${elapsed}ms`);
       console.log(`Sisa Waktu ${remainingTime}ms`);
       console.log("==========================");
       await new Promise((resolve) => setTimeout(resolve, remainingTime));
     }
 
     client.close(() => {
-      connectAndReadDataSequentially();
+      main();
     });
   } catch (error) {
     console.error("Error:", error.message);
+    client.close(() => {
+      main();
+    });
   }
 }
 
-connectAndReadDataSequentially();
+main();
